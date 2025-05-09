1786514fca83403eec0851e06debc79ad1d384fe
aziz <azizcode92@gmail.com>
2025-05-09 19:01:00 +0200

    TypeScript

---
+++ app.ts
@@ -0,0 +1,61 @@
+var userName: string = "Baktash jan"
+var age : number = 26
+var collegae : string = "Baktash IT Center"
+
+console.log(userName)
+console.log(age)
+console.log(collegae)
+
+console.log("-----------add number with data types---------------")
+console.log("Number Data Type")
+var num1 : number = 10;
+
+var num2: number = 32;
+
+var total = num1 + num2
+console.log(total)
+
+console.log("-----------binary and hexadecimal numbers---------------")
+
+var oct:number =0o100001
+var hexa: number = 0b0011
+var binary:number = 0x0001;
+console.log(oct+1)
+console.log(hexa+1)
+console.log(binary+1)
+
+console.log("----------- Convert string to number --------------")
+
+var item : number=100
+var item2="50"
+
+var itemConverted = Number(item2)
+console.log(item+itemConverted)
+console.log(item + item2)
+
+console.log("---------------- Type inference with number -------------------")
+
+let preis = 19.99; // TypeScript erkennt automatisch: preis ist eine Zahl (number)
+
+console.log(typeof preis); // Ausgabe: "number"
+
+console.log("---------------- Decimal with number data type -------------------")
+let preis1: number = 19.99; // Dezimalzahl
+let steuer: number = 0.19;
+
+let gesamt = preis1 + (preis1 * steuer);
+
+console.log("Gesamtpreis:", gesamt); // 👉 23.7881
+
+console.log("---------------	Union  -----------------------")
+let data: number | string;
+
+data = 42;         // ✅ erlaubt
+data = "Hallo";    // ✅ auch erlaubt
+
+
+
+
+
+
+
