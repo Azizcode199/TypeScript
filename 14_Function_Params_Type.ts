8bd6250d0ce409ed56164b679d54aa517b8d9e36
aziz <azizcode92@gmail.com>
2025-05-13 18:21:30 +0200

    Function Params und Return

--- 14_Function_Params_Type.ts
+++ 14_Function_Params_Type.ts
@@ -38,4 +38,34 @@ zeigeBeliebig({ name: "Anna", alter: 30 });
 
 // Boolean
 zeigeBeliebig(true);
+console.log("--------------------------------------------")
 
+const showExamInfo = (id: number, name: string): void => {
+  console.log(`📘 Deine Prüfungs-ID ist: ${id}`);
+  console.log(`👋 Willkommen, Herr/Frau: ${name}!`);
+};
+
+showExamInfo(1, 'Anna');
+
+console.log('-----------isPalindrome--------------')
+
+function isPalindrome(str: string): boolean {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let left = 0;
+  let right = cleaned.length - 1;
+
+  while (left < right) {
+    if (cleaned[left] !== cleaned[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+console.log(isPalindrome("otto"));       // true
+console.log(isPalindrome("Anna"));       // true
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("hello"));      // false
